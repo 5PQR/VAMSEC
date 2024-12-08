1e0fb4b2d1fa56e779267cbf13207a857a03de1d
SPQR_AETERNUM <122448634+5PQR@users.noreply.github.com>
2024-12-08 15:47:09 +0200

    Update config.py

--- SPQR.VAMSEC.December24/config.py
+++ SPQR.VAMSEC.December24/config.py
@@ -1,6 +1,15 @@
 #update your vam path here like vam_path = 'D:\\Downloads\\vam1.22.0.4\\'
-vam_path = 'D:\\Downloads\\vam1.22.0.4\\'
+vam_path = 'PUT YOUR VAM PATH HERE'
 
 
 #what to look for
 search_for = 'OpenURL'
+
+highlights = [
+    'patreon.com',
+    'github.com',
+    'afdian.net', 
+    'voxta.ai', 
+    'discord.gg',
+    'virtamate.com'
+    ]
